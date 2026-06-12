1b8d3f0af85f0027eb1ba3f937059fc8567ec083
Andrea Saderi <andrea.saderi16@gmail.com>
2026-06-12 14:08:53 +0200

    before avoiding inizialization of games

--- src/main/java/andreasaderi/Application.java
+++ src/main/java/andreasaderi/Application.java
@@ -7,11 +7,15 @@ import andreasaderi.entities.Videogioco;
 import andreasaderi.entities.enums.Genre;
 import andreasaderi.entities.enums.Platform;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
 //        10 Giochi:
 
@@ -24,6 +28,10 @@ public class Application {
         Gioco giocoDaTavolo3 = new GiocoDaTavolo("Ticket to Ride", 2004, 45, 5, 60);
         Gioco giocoDaTavolo4 = new GiocoDaTavolo("Monopoli", 1980, 30, 4, 200);
         Gioco giocoDaTavolo5 = new GiocoDaTavolo("Pandemic", 2008, 42, 4, 60);
+
+        List<Gioco> allTheGames = new ArrayList<>(List.of(videogioco1, videogioco2, videogioco3, videogioco4, giocoDaTavolo1, giocoDaTavolo2, giocoDaTavolo3, giocoDaTavolo4, giocoDaTavolo5));
+        System.out.println("All the games: " + allTheGames);
+
 
 //        Collezioni:
 
@@ -46,6 +54,32 @@ public class Application {
 //        collezione1.updateGame(1, giocoDaTavolo3);
 //        System.out.println(collezione1);
 //        collezione1.printCollectionStats();
+
+
+//        Interazione utente:
+        while (true) {
+            System.out.println("\nInserisci il numero corrispondente e premi invio (inserisci 0 per uscire)");
+            int input = -1;
+            try {
+                input = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Formato errato, inserisci un numero!");
+                continue;
+            }
+            if (input == 0) break;
+            switch (input) {
+                case 1: {
+                    System.out.println("Hai digitato 1");
+                    break;
+                }
+                case 2: {
+                    System.out.println("Hai digitato 2");
+                    break;
+                }
+                default:
+                    System.out.println("Input non valido");
+            }
+        }
 
     }
 }
